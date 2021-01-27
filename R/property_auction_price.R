fd9c51112a3dec277d6f2815e7034792bc859923
Tina Ye <xye112@aucklanduni.ac.nz>
2021-01-28 12:49:41 +1300

    improved version. get_property_auction_price

---
+++ R/property_auction_price.R
@@ -0,0 +1,97 @@
+install.packages("rvest")
+install.packages("dplyr")
+install.packages("lubridate")
+install.packages("tidyverse")
+install.packages("purrr")
+
+library(rvest)
+library(dplyr)
+library(lubridate)
+library(tidyverse)
+library(purrr)
+
+#get_one_page function
+get_one_page <- function(url){
+  page <- read_html(url)
+  enclosing_nodes <-  html_nodes(page,'.padb-property-card')
+  df <- map_dfr(enclosing_nodes,~list(
+    property_address=get_html_text(.x,".address"),
+    auction_price=get_html_text(.x, ".padb-property-value"),
+    auction_dates=get_html_text(.x,".padb-auction-details"),
+    bedrooms=replace_blank_with_na(.x,".padb-beds"),
+    bathrooms=replace_blank_with_na(.x,".padb-baths"),
+    car_parking=replace_blank_with_na(.x,".padb-parking"),
+    rating_value=replace_blank_with_na(.x,".padb-property-rating-value , .padb-rating-date"),
+    rating_dates=replace_blank_with_na(.x,".padb-rating-date"))
+  )
+  df <- mutate(
+    df,
+    auction_price=as.numeric(gsub("[^[:digit:]]", "", auction_price)),
+    auction_dates=dmy(auction_dates),
+    bedrooms=as.numeric(bedrooms),
+    bathrooms=as.numeric(bathrooms),
+    car_parking=as.numeric(car_parking),
+    rating_value=as.numeric(gsub("[^[:digit:]]", "", lapply(rating_value, get_rating_value))),
+    rating_dates=str_sub(my(rating_dates),1,7)
+  )
+  df
+}
+
+
+
+#get_rating_value function
+get_rating_value <- function(rating_value){
+  str_split(rating_value," ")[[1]][3]
+}
+
+#get_property_auction_price function
+get_property_auction_price <- function(region=NULL,district=NULL,area=NULL){
+  num <- 1:ceiling(get_number_of_results(region=region)/25)
+  urls <- paste0("https://www.interest.co.nz/property/residential-auction-results?",
+                 "region=",replace_space_with_20(replace_null(region)),
+                 "&district=",replace_space_with_20(replace_null(district)),
+                 "&area=",replace_space_with_20(replace_null(area)),
+                 "&agency=-",
+                 "&status=Sold&page=",num)
+  bind_rows(lapply(urls, get_one_page))
+}
+
+#replace null with "-" function
+replace_null <- function(region=NULL){
+  if (is.null(region)){
+    return(region <- "-")
+  }
+  region
+}
+
+#get_number_of_results function
+get_number_of_results <- function(region=NULL){
+  if (is.null(region)){
+    get_number(replace_null(region))
+  }
+  get_number(replace_space_with_20(region))
+}
+
+#get_number function
+get_number <- function(region){
+  num_url <- paste0("https://www.interest.co.nz/property/residential-auction-results?region=",
+                    region,"&district=-&area=-&agency=-&status=Sold")
+  num_page <- read_html(num_url)
+  results <- html_text(html_node(num_page, '#padb-query-message'))
+  number <- as.numeric(gsub("[^[:digit:]]", "", results))
+  return(number)
+}
+
+
+
+# get_html_text function
+get_html_text <- function(x,y) {
+  html_text( html_node(x,y),trim = TRUE)
+}
+
+#replace blank with na function
+replace_blank_with_na <- function(x,y){
+  {if(length(get_html_text(x,y)) == 0) NA else get_html_text(x,y)}
+}
+
+get_property_auction_price(region = "Bay of Plenty",district = ,area=)
