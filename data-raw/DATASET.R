14d6d4141efd328d4e018457aaa066fb1e7fc88c
Tina Ye <xye112@aucklanduni.ac.nz>
2021-02-11 13:23:52 +1300

    plots updated add lon&lat dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,37 @@
 ## code to prepare `DATASET` dataset goes here
-nzhousingprice <- get_property_auction_price(region = ,district = ,area=)
-usethis::use_data(nzhousingprice, overwrite = TRUE)
+nzhousingprice <- get_property_auction_price(region = , district = , area = )
 
+nzhousingprice_clear <- filter(nzhousingprice, year(auction_dates) > 2017, bedrooms <= 13 | is.na(bedrooms), bathrooms <= 10 | is.na(bathrooms)) %>%
+  mutate(
+    region = replace(region, property_address == "Woolston, 46 Mackenzie Avenue ST7730", "Canterbury"),
+    region = replace(region, property_address == "Fendalton, 39B Kotare Street PI65339", "Canterbury"),
+    region = replace(region, property_address == "4 Glenbush Place, Birkenhead", "Auckland"),
+    region = replace(region, property_address == "Sunnynook, 27 Lyford Crescent GL42583", "Auckland"),
+    region = replace(region, property_address == "Papatoetoe, 23 Clendon Avenue HO3828", "Auckland"),
+    district = replace(district, property_address == "17 Glasgow Crescent, Kaiti", "Gisborne"),
+    district = replace(district, property_address == "Beach Haven, 3/2 McGlashen Place MJ45829", "North Shore City"),
+    district = replace(district, property_address == "30 Arthur Street, Ellerslie", "Auckland City"),
+    district = replace(district, property_address == "163 Bellevue Road", "Tauranga"),
+    district = replace(district, property_address == "4 Glenbush Place, Birkenhead", "North Shore City"),
+    district = replace(district, property_address == "13 Franklin Road, Freemans Bay", "Auckland City"),
+    district = replace(district, property_address == "Sunnynook, 27 Lyford Crescent GL42583", "North Shore City"),
+    district = replace(district, property_address == "Papatoetoe, 23 Clendon Avenue HO3828", "Manukau City"),
+    district = replace(district, property_address == "38 Waimarie Street, St Heliers", "Auckland City"),
+    auction_price = replace(auction_price, auction_price == 12017200001202, 720000),
+    auction_price = replace(auction_price, auction_price == 240000270000, 255000),
+    auction_price = replace(auction_price, auction_price == 2957, NA),
+    auction_price = replace(auction_price, auction_price == 678391, NA)
+  )
+
+nzhousingprice_geocoded <- mutate(nzhousingprice_clear,
+  property_address = str_c(property_address, district, region, " New Zealand", sep = ",")
+) %>%
+  mutate_geocode(property_address)
+geocoded_nz <- mutate(nzhousingprice_geocoded,
+  lon = replace(lon, lon < 0, NA),
+  lat = replace(lat, lat > 0, NA)
+) %>%
+  select(property_address, lon, lat)
+
+usethis::use_data(nzhousingprice, overwrite = TRUE)
+usethis::use_data(geocoded_nz)
--- plot/auction_price_plot.R
+++ plot/auction_price_plot.R
@@ -1,17 +1,16 @@
 #install.packages("leaflet")
 #install.packages("zoo")
+#install.packages("ggridges")
 library(ggmap)
 library(leaflet)
 library(zoo)
 library(stringr)
 library(lubridate)
+library(ggridges)
 
-add_month <- nzhousingprice
-add_month <- mutate(add_month, auction_dates = as.yearmon(auction_dates))
-add_month <- mutate(add_month,auction_price =replace(auction_price,auction_price== 12017200001202 , 720000),
-                              auction_price = replace(auction_price,auction_price== 240000270000 ,255000),
-                              auction_price = replace(auction_price,auction_price== 2957, 2900000),
-                              auction_price = replace(auction_price,auction_price==678391 ,6780000))
+
+
+add_month <- mutate(nzhousingprice_clear, auction_dates = as.yearmon(auction_dates))
 # map for auction property
 register_google(key = "")
 filter(add_month, region %in% c("Auckland"), auction_price >= 5000000) %>%
@@ -36,10 +35,7 @@ geocoded %>%
 # map for auction_price per region
 dataset <- add_month %>%
   mutate(group = ifelse(region == "Auckland", "Auckland", "Others")) %>%
-  group_by(auction_dates, group) %>%
-  filter(group %in% c("Auckland", "Others") & auction_dates != "Jul 2016"
-  & auction_dates != "Jul 2017"
-  & auction_dates != "Sep 2017")
+  group_by(auction_dates, group)
 dataset_auction_price <- summarise(dataset, med = median(auction_price, na.rm = TRUE))
 ggplot(dataset_auction_price, aes(auction_dates, med)) +
   geom_line(aes(color = group), size = 1) +
@@ -53,7 +49,7 @@ ggplot(dataset_count, aes(auction_dates, counts)) +
   geom_point(aes(color = factor(group))) +
   scale_color_manual(values = c("#0073C2FF", "#EFC000FF"))
 
-# plot for counts per region  over months
+# plot for counts of per year over months
 dataset_month <- mutate(dataset, month = month(auction_dates), year = year(auction_dates)) %>%
   group_by(month, year) %>%
   summarise(counts = n())
@@ -64,35 +60,57 @@ ggplot(dataset_month, aes(factor(month), counts, group = factor(year))) +
   theme(legend.position = "top") +
   scale_color_manual(values = c("#ffd080", "#ff6666", "#cc6600", "#660000"))
 
-# plot for counts weekday v.s weekend
-dataset_day <- mutate(nzhousingprice, year = year(auction_dates), day = weekdays(auction_dates)) %>%
-  mutate(group = ifelse(day %in% c("Sunday", "Saturday"), "weekend", "weekday")) %>%
-  group_by(group, year) %>%
-  summarise(counts = n()) %>%
-  filter(year != "2016"
-  & year != "2017")
-ggplot(dataset_day, aes(factor(year), counts)) +
-  geom_bar(aes(color = group, fill = group),
+# plot for counts Auckland vs Others  over months
+dataset_group <- mutate(dataset, month = month(auction_dates), year = year(auction_dates)) %>%
+  group_by(month, year, group) %>%
+  summarise(counts = n())
+ggplot(dataset_group, aes(factor(month), counts, group = factor(year))) +
+  geom_line(aes(color = factor(year)), size = 1) +
+  geom_point(aes(color = factor(year))) +
+  labs(x = "Month", colour = "Year") +
+  theme(legend.position = "top") +
+  scale_color_manual(values = c("#ffd080", "#ff6666", "#cc6600", "#660000")) +
+  facet_wrap(~group)
+
+# plot for counts per day
+dataset_day <- mutate(nzhousingprice_clear, year = year(auction_dates), day = factor(weekdays(auction_dates),
+  levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+)) %>%
+  group_by(day) %>%
+  summarise(counts = n())
+ggplot(dataset_day, aes(day, counts)) +
+  geom_bar(aes(color = day, fill = day),
     stat = "identity", position = position_dodge(0.8),
     width = 0.7
-  ) +
-  scale_color_manual(values = c("#ffd080", "#ff6666")) +
-  scale_fill_manual(values = c("#ffd080", "#ff6666")) +
-  geom_text(aes(label = counts, group = group),
-    position = position_dodge(0.8),
-    vjust = -0.3, size = 3.5
   )
 
 
 
 # heatmap
-filter(dataset, !is.na(auction_price)) %>%
+group_by(add_month, bedrooms, bathrooms) %>%
+  summarise(counts = n()) %>%
   ggplot(aes(factor(bedrooms), factor(bathrooms))) +
-  geom_tile(aes(fill = auction_price)) +
-  # scale_fill_continuous(limits=c(0, 8000000), breaks=seq(0,8000000,by=1000000))+
+  geom_tile(aes(fill = counts)) +
   labs(x = "Bedrooms", y = "Bathrooms")
 
 # box plot
 
 ggplot(dataset, aes(factor(group), auction_price)) +
   geom_boxplot()
+
+# Auckland district density plot
+dataset_auckland <- filter(nzhousingprice_clear, !is.na(auction_price), year(auction_dates) == 2020, region == "Auckland")
+ggplot(dataset_auckland, aes(auction_price, district, fill = district)) +
+  geom_density_ridges() +
+  theme_ridges()
+
+# Auckland district line plot
+dataset_auckland_price <- filter(add_month, region == "Auckland") %>%
+  group_by(district, auction_dates) %>%
+  summarise(med = median(auction_price, na.rm = TRUE))
+ggplot(dataset_auckland_price, aes(auction_dates, med, group = district)) +
+  geom_line(aes(color = district), size = 1) +
+  geom_point(aes(color = district)) +
+  labs(x = "Auction Dates", colour = "District") +
+  theme(legend.position = "top") +
+  facet_wrap(~district)
